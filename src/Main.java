25678f3e7816f2582f790aa37684d01bd3f3b74e
GayratAshirov <ashirov.dev@gmail.com>
2024-08-24 17:59:00 +0500

    hello so'zini ekranga chiqaruvchi kod yozdim

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello");
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
     }
 
 }
